21de9be20a11c0b91af0058095c776b472133e71
Koakovski <hkoakovskip@gmail.com>
2024-03-06 19:25:48 -0300

    feat: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  plugins: [tsconfigPaths(), swc.vite()],
+});
