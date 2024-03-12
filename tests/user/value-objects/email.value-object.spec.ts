6bc404f99e16747643eef26b3feef6c8909528d8
Koakovski <hkoakovskip@gmail.com>
2024-03-12 19:42:14 -0300

    test: refactor names

--- tests/user/value-objects/email.value-object.spec.ts
+++ tests/user/value-objects/email.value-object.spec.ts
@@ -2,17 +2,17 @@ import { EmailValueObject } from 'domain/user/value-objects/email.value-object';
 
 describe('EmailValueObject', () => {
   it('should return a valid email', () => {
-    const email = EmailValueObject.create('valid_mail@domain.com');
-    expect(email.isFail()).toBe(false);
+    const result = EmailValueObject.create('valid_mail@domain.com');
+    expect(result.isFail()).toBe(false);
   });
 
   it('should return fail if providaded a invalid email', () => {
-    const email = EmailValueObject.create('invalid_mail');
-    expect(email.isFail()).toBe(true);
+    const result = EmailValueObject.create('invalid_mail');
+    expect(result.isFail()).toBe(true);
   });
 
   it('should normalize the email to lower case', () => {
-    const email = EmailValueObject.create('Valid_Mail@DOMAin.coM');
-    expect(email.value().get('value')).toBe('valid_mail@domain.com');
+    const result = EmailValueObject.create('Valid_Mail@DOMAin.coM');
+    expect(result.value().get('value')).toBe('valid_mail@domain.com');
   });
 });
