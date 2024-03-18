cc025f45ea23fb58018d7afd0fa8397c12206a78
Koakovski <hkoakovskip@gmail.com>
2024-03-18 19:26:58 -0300

    feat: add AcceptedAtValueObject

---
+++ domain/user/value-objects/accepted-at.value-object.ts
@@ -0,0 +1,25 @@
+import { IResult, Result, ValueObject } from 'types-ddd';
+import { format, isDate } from 'date-fns';
+
+export type AcceptedAtValueObjectProps = {
+  value: Date;
+};
+
+export class AcceptedAtValueObject extends ValueObject<AcceptedAtValueObjectProps> {
+  private constructor(props: AcceptedAtValueObjectProps) {
+    super(props);
+  }
+
+  get value(): string {
+    return format(this.props.value, 'yyyy-MM-dd hh:mm:ss');
+  }
+
+  public static create(date: Date): IResult<AcceptedAtValueObject> {
+    const isValidDate = isDate(date);
+    if (!isValidDate) {
+      return Result.fail('Invalid Date');
+    } else {
+      return Result.Ok(new AcceptedAtValueObject({ value: date }));
+    }
+  }
+}
