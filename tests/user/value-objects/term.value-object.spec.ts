18cd222598079ea2c8085e4819454880176dbc85
Koakovski <hkoakovskip@gmail.com>
2024-03-18 20:00:18 -0300

    refactor: refactor creation os props in TermValueObject tests

--- tests/user/value-objects/term.value-object.spec.ts
+++ tests/user/value-objects/term.value-object.spec.ts
@@ -1,10 +1,13 @@
 import { AcceptedAtValueObject } from 'domain/user/value-objects/accepted-at.value-object';
 import { IpValueObject } from 'domain/user/value-objects/ip.value-object';
-import { TermValueObject } from 'domain/user/value-objects/term.value-object';
+import {
+  TermValueObject,
+  TermValueObjectCreateProps,
+} from 'domain/user/value-objects/term.value-object';
 
 describe('TermValueObject', () => {
-  it('should create a valid term', () => {
-    const result = TermValueObject.create({
+  function makeProps(): TermValueObjectCreateProps {
+    return {
       ip: IpValueObject.create('123.123.123.123').value(),
       acceptedAt: AcceptedAtValueObject.create(
         new Date('2020-01-02 10:00:00'),
@@ -15,24 +18,20 @@ describe('TermValueObject', () => {
         os: 'Linux',
         type: 'browser',
       },
-    });
+    };
+  }
+
+  it('should create a valid term', () => {
+    const props = makeProps();
+    const result = TermValueObject.create(props);
 
     expect(result.isFail()).toBe(false);
   });
 
   it('should fail if provide an invalid OS', () => {
-    const result = TermValueObject.create({
-      ip: IpValueObject.create('123.123.123.123').value(),
-      acceptedAt: AcceptedAtValueObject.create(
-        new Date('2020-01-02 10:00:00'),
-      ).value(),
-      userAgent: {
-        name: 'firefox',
-        version: '86.0.0',
-        os: 'invalid_Os',
-        type: 'browser',
-      },
-    });
+    const props = makeProps();
+    props.userAgent.os = 'invalid_os';
+    const result = TermValueObject.create(props);
 
     expect(result.isFail()).toBe(true);
     expect(result.error()).toBe('Invalid OS');
